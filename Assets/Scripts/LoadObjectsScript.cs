e8765924479128c89cb86d4cd945805d9e6d73aa
Calliste V <callistevrks@gmail.com>
2019-03-05 16:51:19 +0100

    Initial commit

---
+++ Assets/Scripts/LoadObjectsScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore.Examples.HelloAR;
+using GoogleARCore.Examples.ObjectManipulation;
+using UnityEngine;
+
+public class LoadObjectsScript : MonoBehaviour
+{
+
+    public AndyPlacementManipulator arManipulator;
+    public HelloARController arController;
+    public GameObject object3d;
+    
+    // Start is called before the first frame update
+
+    public void LoadObject()
+    {
+      //  arController.AndyPlanePrefab = object3d;
+        arManipulator.AndyPrefab = object3d;
+    }
+}
