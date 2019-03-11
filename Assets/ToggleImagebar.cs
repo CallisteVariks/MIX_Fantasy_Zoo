7af72e8b14113585f6c1bd9be6f4555ddeed16a0
Calliste V <callistevrks@gmail.com>
2019-03-11 18:52:47 +0100

    Scroll View added
    New models plus scroll view added.

--- Assets/ToggleImagebar.cs
+++ Assets/ToggleImagebar.cs
@@ -1,27 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Vuforia;
 
 public class ToggleImagebar : MonoBehaviour
 {
 
     public GameObject imagebar;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ToggleImage()
     {
-        imagebar.GetComponent<Image>();
-        enabled = false;
+        imagebar.SetActive(!imagebar.activeSelf);
     }
 }
